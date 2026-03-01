bc6b76f9c96d467b49a37e3f8e37560222a2a318
mikan-matsu <akitomatsuyama1@gmail.com>
2026-03-02 00:22:38 +0900

    Fix: Amplify初期化をuseEffect内で実行するよう修正（SSR対応）

--- src/app/configure-amplify.tsx
+++ src/app/configure-amplify.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 import awsExports from '../aws-exports';
 
-Amplify.configure(awsExports, { ssr: true });
-
 export default function ConfigureAmplifyClient() {
+  useEffect(() => {
+    Amplify.configure(awsExports, { ssr: true });
+  }, []);
+
   return null;
 }
